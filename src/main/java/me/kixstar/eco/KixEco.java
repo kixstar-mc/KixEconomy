fc3d70f5c6eb295931df0ef14c548026ce7c4663
strajabot <Strajabot@gmail.com>
2020-07-02 09:12:43 +0200

    Implement players losing a configurable percentage of money on death

--- src/main/java/me/kixstar/eco/KixEco.java
+++ src/main/java/me/kixstar/eco/KixEco.java
@@ -1,16 +1,26 @@
 package me.kixstar.eco;
 
 import me.kixstar.eco.listener.CoinpouchListener;
+import me.kixstar.eco.listener.PlayerDeathListener;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.Plugin;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class KixEco extends JavaPlugin {
 
+
+    private static RegisteredServiceProvider<Economy> economyProvider;
+
     private static final Plugin instance = Bukkit.getServer().getPluginManager().getPlugin("KixEco");
 
     public void onEnable() {
+        KixEco.economyProvider = getServer().getServicesManager().getRegistration(Economy.class);
+
         this.getServer().getPluginManager().registerEvents(new CoinpouchListener(), this);
+        this.getServer().getPluginManager().registerEvents(new PlayerDeathListener(), this);
+
     }
 
     public void onDisable() {
@@ -20,5 +30,7 @@ public class KixEco extends JavaPlugin {
     public static KixEco getInstance() {
         return null == instance ? null : (KixEco) instance;
     }
+
+    public static Economy getEconProvider() {return KixEco.economyProvider.getProvider();}
 
 }
